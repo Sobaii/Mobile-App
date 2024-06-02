e8fd1d3400e0de32377b1f8d57259eec49434430
Dan Li <danmuxingli@gmail.com>
2024-06-02 16:20:33 -0400

    feat: using AsyncStorage for mvp demo

--- sobaii-mobile/store/index.ts
+++ sobaii-mobile/store/index.ts
@@ -1,24 +1,35 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// TODO implement MMKV persistent storage in expo
 import { persistReducer, persistStore } from "redux-persist";
 import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 
 import logger from "redux-logger";
 import authReducer from "./reducers/authSlice";
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
 const rootReducer = combineReducers({
     auth: authReducer
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 // Configure store
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(logger),
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }).concat(logger),
 });
 
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
