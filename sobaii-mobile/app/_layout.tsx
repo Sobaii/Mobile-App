ab3fa4cca133153b03fc89e55d58154faef2940d
Dan Li <danmuxingli@gmail.com>
2024-07-19 21:22:12 -0400

    chore: add stripe react native sdk wrapper

--- sobaii-mobile/app/_layout.tsx
+++ sobaii-mobile/app/_layout.tsx
@@ -8,15 +8,17 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 import { NavLightTheme, NavDarkTheme } from '@/constants/Colors';
 import { RootSiblingParent } from 'react-native-root-siblings';
 import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo"
+import { StripeProvider } from '@stripe/stripe-react-native';
 import { tokenCache } from '@/lib/clerk/tokenCache';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+const clerkPublishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+const stripePublishableKey = process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY!;
 
-if (!publishableKey) {
-  throw new Error('Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env')
+if (!clerkPublishableKey || !stripePublishableKey) {
+  throw new Error('Missing Publishable Keys. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY, EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY in your .env')
 }
 
 export default function RootLayout() {
@@ -36,23 +38,25 @@ export default function RootLayout() {
   }
 
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={clerkPublishableKey}>
       <ClerkLoaded>
-        <ThemeProvider value={colorScheme === 'dark' ? NavDarkTheme : NavLightTheme}>
-          <RootSiblingParent>
-            <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              <Stack.Screen name="sign-in" options={{ headerShown: false }} />
-              <Stack.Screen name="sign-up" options={{ headerShown: false }} />
-              <Stack.Screen name="camera-screen" options={{ headerShown: false }} />
-              <Stack.Screen name='expense-manager-screen' />
-              <Stack.Screen name='identity-configuration-screen' />
-              <Stack.Screen name='app-settings-screen' />
-              <Stack.Screen name='wallet-screen' />
-              <Stack.Screen name="+not-found" />
-            </Stack>
-          </RootSiblingParent>
-        </ThemeProvider>
+        <StripeProvider publishableKey={stripePublishableKey}>
+          <ThemeProvider value={colorScheme === 'dark' ? NavDarkTheme : NavLightTheme}>
+            <RootSiblingParent>
+              <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+                <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+                <Stack.Screen name="camera-screen" options={{ headerShown: false }} />
+                <Stack.Screen name='expense-manager-screen' />
+                <Stack.Screen name='identity-configuration-screen' />
+                <Stack.Screen name='app-settings-screen' />
+                <Stack.Screen name='wallet-screen' />
+                <Stack.Screen name="+not-found" />
+              </Stack>
+            </RootSiblingParent>
+          </ThemeProvider>
+        </StripeProvider>
       </ClerkLoaded>
     </ClerkProvider>
   );
